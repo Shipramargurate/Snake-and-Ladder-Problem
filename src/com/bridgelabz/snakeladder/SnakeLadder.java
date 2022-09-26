ac7180c122166e4d860cd1ff9aa7eb4593fcf3e5
shipramargurate <shivanikumari22322@gmail.com>
2022-09-27 00:03:09 +0530

    Added : position of player 1

--- src/com/bridgelabz/snakeladder/SnakeLadder.java
+++ src/com/bridgelabz/snakeladder/SnakeLadder.java
@@ -1,9 +1,11 @@
 package com.bridgelabz.snakeladder;
 
 public class SnakeLadder {
-
+//Snake and Ladder game played with single player at start position 0
 	public static void main(String[] args) {
 		System.out.println("welcome to snake and ladder game program");
+		int position = 0;
+		System.out.println("position of player 1 is : " + position);
 	}
 
 }
