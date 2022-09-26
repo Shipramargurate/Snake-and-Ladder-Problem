953e99b5d39cf0b7f0aebcf56e30ebc8cdab0e67
shipramargurate <shivanikumari22322@gmail.com>
2022-09-27 00:13:22 +0530

    Added : Fuctionality of rolling dice

--- src/com/bridgelabz/snakeladder/SnakeLadder.java
+++ src/com/bridgelabz/snakeladder/SnakeLadder.java
@@ -1,11 +1,16 @@
 package com.bridgelabz.snakeladder;
 
+import java.util.Random;
+
 public class SnakeLadder {
-//Snake and Ladder game played with single player at start position 0
+//The Player rolls the die to get a number between 1 to 6.
 	public static void main(String[] args) {
 		System.out.println("welcome to snake and ladder game program");
-		int position = 0;
+		int position = 0; 
 		System.out.println("position of player 1 is : " + position);
+		Random random = new Random(); //creating random object
+		int roll = 1 + random.nextInt(6); // 1 to 6
+		System.out.println("after rolling dice : " + roll);
 	}
 
 }
