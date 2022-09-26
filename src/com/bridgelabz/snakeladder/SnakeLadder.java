90f82f870dc7f4abe817934711952adc8a8a514b
shipramargurate <shivanikumari22322@gmail.com>
2022-09-26 23:50:09 +0530

    Added : Welcome to Snake and Ladder program

---
+++ src/com/bridgelabz/snakeladder/SnakeLadder.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.snakeladder;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to snake and ladder game program");
+	}
+
+}
